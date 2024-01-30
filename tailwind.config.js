e82e62d58189f7488a923a2fb141b7a48c417a21
Azzedine zemmari <98091150+Azzedine-zemmari@users.noreply.github.com>
2024-01-30 18:38:59 +0100

    ..

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html","./src/**/*.{html,js,vue}"],
+  purge: ["./index.html","./src/**/*.{html,vue}"],
   theme: {
     extend: {},
   },
